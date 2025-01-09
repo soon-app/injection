6f2ff06dbe11cf18d0f28d80a46e4a7e1605ee5e
Rémi <78747335+remimd@users.noreply.github.com>
2025-01-09 14:45:20 +0100

    feat: ✨ `load_modules_with_keywords` function

--- tests/utils/test_load_packages.py
+++ tests/utils/test_load_packages.py
@@ -3,53 +3,53 @@ import pytest
 from injection.utils import load_packages
 
 
-class TestLoadPackage:
-    def test_load_package_with_predicate(self):
-        from tests.utils import package
+class TestLoadPackages:
+    def test_load_packages_with_predicate(self):
+        from tests.utils import package1
 
         loaded_modules = load_packages(
-            package,
+            package1,
             predicate=lambda name: ".excluded_package." not in name,
         )
 
-        assert "tests.utils.package.excluded_package.module3" not in loaded_modules
+        assert "tests.utils.package1.excluded_package.module3" not in loaded_modules
 
         modules = (
-            "tests.utils.package.module1",
-            "tests.utils.package.sub_package.module2",
+            "tests.utils.package1.module1",
+            "tests.utils.package1.sub_package.module2",
         )
 
         for module in modules:
             assert module in loaded_modules
 
-    def test_load_package_with_success(self):
-        from tests.utils import package
+    def test_load_packages_with_success(self):
+        from tests.utils import package1
 
-        loaded_modules = load_packages(package)
+        loaded_modules = load_packages(package1)
 
         modules = (
-            "tests.utils.package.module1",
-            "tests.utils.package.sub_package.module2",
-            "tests.utils.package.excluded_package.module3",
+            "tests.utils.package1.module1",
+            "tests.utils.package1.sub_package.module2",
+            "tests.utils.package1.excluded_package.module3",
         )
 
         for module in modules:
             assert module in loaded_modules
 
-    def test_load_package_with_str(self):
-        loaded_modules = load_packages("tests.utils.package")
+    def test_load_packages_with_str(self):
+        loaded_modules = load_packages("tests.utils.package1")
 
         modules = (
-            "tests.utils.package.module1",
-            "tests.utils.package.sub_package.module2",
-            "tests.utils.package.excluded_package.module3",
+            "tests.utils.package1.module1",
+            "tests.utils.package1.sub_package.module2",
+            "tests.utils.package1.excluded_package.module3",
         )
 
         for module in modules:
             assert module in loaded_modules
 
-    def test_load_package_with_module_raise_type_error(self):
-        from tests.utils.package import module1
+    def test_load_packages_with_module_raise_type_error(self):
+        from tests.utils.package1 import module1
 
         with pytest.raises(TypeError):
             load_packages(module1)
